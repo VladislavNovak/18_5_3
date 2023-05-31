165db76ae2456f357a9d7cc3b0084e47f791d36a
Novak <novaldix@gmail.com>
2023-05-31 21:46:53 +0300

    18_5_3 added solution

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
-int main() {
+using std::cout;
+using std::endl;
 
+int getNumberOfJumps(int step, int maxJump = 3) {
+    if (step <= 0) {
+        return step == 0 ? 1 : 0;
+    }
+
+    int variables = 0;
+    for (int i = maxJump; i > 0; --i) {
+        int temp = getNumberOfJumps(step - i, maxJump);
+        variables += temp;
+    }
+
+    return variables;
+}
+
+int main() {
+    const unsigned int stairRung = 4;
+    const unsigned int maxJump = 2;
+
+    int result = getNumberOfJumps(stairRung, maxJump);
+    cout << "Result is: " << result << endl;
 }
